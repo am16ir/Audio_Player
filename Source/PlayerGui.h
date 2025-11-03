3f499633f4baf6da53c711ce4982a62449d9256a
Abdelmalik Nabil <abdelmalknabil6@gmail.com>
2025-11-03 19:55:40 +0200

    Update PlayerGui.h

--- Source/PlayerGui.h
+++ Source/PlayerGui.h
@@ -1,12 +1,49 @@
 #pragma once
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
+#include <vector>
+
+using namespace std;
+
+class tablemodel : public juce::TableListBoxModel {
+
+public:
+	function<void(juce::String)> doubleClick;
+	vector<pair<juce::String, juce::String>> files;
+	tablemodel() = default;
+	void paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool rowIsSelected) override {
+		if (columnId == 1) {
+			juce::File f(files[rowNumber].first);
+			g.drawText(f.getFileName(), juce::Rectangle<int>(0, 0, width, height), juce::Justification::centredLeft);
+		}
+		else if (columnId == 2)
+			g.drawText((files[rowNumber].second), juce::Rectangle<int>(0, 0, width, height), juce::Justification::centredLeft);
+	}
+
+	int getNumRows() override {
+		return files.size();
+	}
+	void paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected) override {
+		if (rowIsSelected)
+			g.fillAll(juce::Colours::lightblue);
+		else
+			g.fillAll(juce::Colours::grey);
+	}
+	void cellDoubleClicked(int rowNumber, int columnId, const juce::MouseEvent& e) override {
+		if (rowNumber >= 0 && rowNumber < files.size()) {
+			juce::String filename = files[rowNumber].first;
+			if (doubleClick)
+				doubleClick(filename);
+		}
+	}
+
+};
 
 
-class PlayerGui	: public juce::AudioAppComponent,
-				  public juce::Button::Listener,
-				  public juce::Slider::Listener,
-				  public juce::Timer
+class PlayerGui : public juce::AudioAppComponent,
+	public juce::Button::Listener,
+	public juce::Slider::Listener,
+	public juce::Timer
 {
 private:
 
@@ -20,10 +57,15 @@ private:
 	juce::TextButton backward{ "Backward -10" };
 	juce::TextButton loopButton{ "Loop" };
 	juce::TextButton mute{ "Mute" };
+	juce::TextButton startingpoint{ "Starting point" };
+	juce::TextButton endingpoint{ "Ending point" };
+	juce::TextButton Deletepoints{ "Delete points" };
+	juce::TextButton looponpoints{ "LOOP ON POINTS" };
 	juce::TextEditor statusBox;
 	juce::Slider volumeSlider;
+	juce::TableListBox mytable;
 	juce::Slider positionslider;
-
+	tablemodel model;
 	std::unique_ptr<juce::FileChooser> fileChooser;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGui)
@@ -36,14 +78,14 @@ public:
 	void prepareToPlay(int samplesPerBlockExpected, double sampleRate)override;
 	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)override;
 	void releaseResources()override;
-	
+
 	void paint(juce::Graphics& g) override;
 	void resized() override;
 
 	void buttonClicked(juce::Button* button)override;
 	void sliderValueChanged(juce::Slider* slider)override;
 
-	void timerCallback() override;
-
 	void statusBoxMessage(const juce::String& message);
+
+	void timerCallback() override;
 };
